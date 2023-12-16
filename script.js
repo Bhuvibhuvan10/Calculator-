5404d8d8279224882bf8b76b919f8744314f43bf
Bhuvibhuvan10 <136156171+Bhuvibhuvan10@users.noreply.github.com>
2023-12-16 21:35:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function changecolor(){
+    const btn=document.getElementById('mybtn');
+    btn.style.backgroundColor="blueviolet";
+    btn.style.color="white";
+   
+}
+function greeting(){
+    const grt=document.getElementById('greet');
+    const date=new Date()
+    
+    window.alert(' Welcome to  Smile website '+date);
+
+
+}
+
+
+
